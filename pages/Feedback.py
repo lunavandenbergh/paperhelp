fa816f23108ec66be79121f32bde3e20726ec349
Luna Van den Bergh <luna.vandenbergh@student.kuleuven.be>
2025-03-02 11:38:03 +0100

    Code cleanup

--- pages/Feedback.py
+++ pages/Feedback.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor
 import time
 import pymupdf4llm
 import streamlit as st
@@ -6,26 +7,21 @@ import re
 from src.find_arguments import	generate_arguments
 from src.process_pdf import extract_text_from_pdf
 from src.text_corrections import get_corrections, highlight_text
-from sentence_transformers import SentenceTransformer	
 from together import Together
 import chromadb
 from src.generate_response import generate_response
 from src.process_pdf import create_embeddings, extract_text_from_pdf, split_text, store_embeddings
-import json
 
 st.set_page_config(
     page_title="Scientific Writing: Feedback Tool", 
     page_icon="📄",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="collapsed",
     layout="wide")
 
 tic_overall = time.time()
 
 with open( "src/style.css" ) as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
-
-client = Together(api_key=st.secrets["TOGETHER_API_KEY"])
-chroma_client = chromadb.PersistentClient()
 
 if "feedback_type" not in st.session_state:
     st.session_state["feedback_type"] = "Arguments"
@@ -36,30 +32,28 @@ if "text" not in st.session_state:
     pdf_path = pdf_path.split('uploads\\', 1)[-1]
     new_pdf_path = f"files/{pdf_path}"
     text = pymupdf4llm.to_markdown(new_pdf_path)
-    # Remove newlines within paragraphs
-    text = re.sub(r'(?<!\n)\n(?!\n)', ' ', text)
-    # Join hyphenated words
-    text = re.sub(r'(\w+)-\s+(\w+)', r'\1\2', text)
+    text = re.sub(r'(?<!\n)\n(?!\n)', ' ', text) # Remove newlines within paragraphs
+    text = re.sub(r'(\w+)-\s+(\w+)', r'\1\2', text) # Join hyphenated words
     st.session_state["text"] = text
     toc = time.time()
     print(f"Text extraction took {toc - tic:.2f} seconds")
 
 if "corrections" not in st.session_state:
     tic = time.time()
-    tool = language_tool_python.LanguageTool('en-US')
-    # Join hyphenated words (otherwise they will be treated as separate words and probably incorrect)
+    if "language_tool" not in st.session_state:
+        st.session_state["language_tool"] = language_tool_python.LanguageTool('en-US')
+    tool = st.session_state["language_tool"]
     st.session_state["corrections"] = get_corrections(tool, st.session_state["text"])
     toc = time.time()
     print(f"Text correction took {toc - tic:.2f} seconds")
 
-# Set a default model
 if "togetherai_model" not in st.session_state:
     st.session_state["togetherai_model"] = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+    client = Together(api_key=st.secrets["TOGETHER_API_KEY"])
+    chroma_client = chromadb.PersistentClient()
 
-# Initialize chat history
 if "messages" not in st.session_state:
-    st.session_state.messages = []
-    st.session_state.messages.append({"role": "assistant", "content": "Welcome! How can I help you? You can ask me anything about the paper you provided or anything else."})
+    st.session_state.messages = [{"role": "assistant", "content": "Welcome! How can I help you? You can ask me anything about the paper you provided or anything else."}]
 
 if "arguments" not in st.session_state:
     tic = time.time()
@@ -68,31 +62,32 @@ if "arguments" not in st.session_state:
     toc = time.time()
     print(f"Argument generation took {toc - tic:.2f} seconds")
 
-#if "embeddings" not in st.session_state:
-    
+if "processpdf" not in st.session_state or st.session_state["processpdf"] == False:
+    tic = time.time()
+    pdf_path = st.session_state.get("pdf_path", None)
+    pdf_text = extract_text_from_pdf(pdf_path)
+    chunks = split_text(pdf_text)
+
+    with ThreadPoolExecutor() as executor:
+        embeddings_future = executor.submit(create_embeddings, chunks)
+        embeddings = embeddings_future.result()
+
+    st.session_state["processpdf"] = True
+
+    pdf_name = pdf_path.replace(" ","")
+    cleaned = ''.join(filter(str.isalnum, pdf_path))[7:17]
+    collection = chroma_client.get_or_create_collection(name=cleaned)
+    store_embeddings(collection, chunks, embeddings, cleaned)
+
+    toc = time.time()
+    print(f"PDF processing + saving embeddings took {toc - tic:.2f} seconds")
 
 st.title("Scientific Writing Feedback Tool")
-
-# Input pdf field
-pdf_path = st.session_state.get("pdf_path", None)
-pdf_text = extract_text_from_pdf(pdf_path)
-chunks = split_text(pdf_text)
-
-embeddings = []
-embeddings = create_embeddings(chunks)
-
-# Chromadb: get/create a collection and store embeddings in it
-pdf_name = pdf_path.replace(" ","")
-cleaned = ''.join(filter(str.isalnum, pdf_path))
-cleaned = cleaned[7:17]
-collection = chroma_client.get_or_create_collection(name=cleaned)
-store_embeddings(collection, chunks, embeddings, cleaned)
 
 left_col, right_col = st.columns(spec=[2,1],border=True)
 with left_col:
     text_container = st.container(height=400)
     with text_container:
-        ## ANNOTATIONS ##
         corrections = st.session_state["corrections"]
         highlighted_text = highlight_text(st.session_state["text"], corrections)
         st.markdown(highlighted_text, unsafe_allow_html=True)
@@ -102,7 +97,6 @@ with left_col:
         for message in st.session_state.messages:
             with st.chat_message(message["role"]):
                 st.write(message["content"])
-    # Accept user input
     if prompt := st.chat_input("Ask me about your pdf!"):
         st.session_state.messages.append({"role": "user", "content": prompt})
         with chat:
@@ -115,8 +109,7 @@ with left_col:
                     def stream_response():
                         for word in response.split(" "):
                             yield word + " "
-                            time.sleep(0.03)
-                            
+                            time.sleep(0.03)         
                 st.write_stream(stream_response())
             st.session_state.messages.append({"role": "assistant", "content": response})
     
