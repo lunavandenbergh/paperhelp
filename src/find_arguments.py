55e69a651d6cc48aad8b5f915d1d25bf07497348
Luna Van den Bergh <luna.vandenbergh@student.kuleuven.be>
2025-03-03 23:16:42 +0100

    Small improvements

--- src/find_arguments.py
+++ src/find_arguments.py
@@ -1,17 +1,11 @@
 import streamlit as st
-import time
-import json
-from together.error import APIConnectionError
 
 def split_text(text, max_length=2000):
     """Splits the text into chunks of max_length."""
     return [text[i:i + max_length] for i in range(0, len(text), max_length)]
 
-def generate_arguments(precalculated, client):
-    if precalculated:
-        with open("arguments.json", "r") as file:
-            data = json.load(file)
-            return data
+def generate_arguments():
+    client = st.session_state["openai_client"]
     system_message = '''You are an advanced argument analysis system. 
                          Given a text, identify each distinct argument and return them in structured JSON format. 
                          Please do not respond with anything other than the JSON format.
@@ -24,13 +18,11 @@ def generate_arguments(precalculated, client):
                              - counterargument: Any opposing viewpoint presented.
                          - feedback: Analysis of the arguments weaknesses, such as logical fallacies, lack of clarity, or weak evidence.
                          - actionable_feedback: Specific steps to improve the argument.'''
-    
+
     text_chunks = split_text(st.session_state["text"])
-    
     messages = [{"role": "system", "content": system_message}]
     for chunk in text_chunks:
         messages.append({"role": "user", "content": chunk})
-            
     stream = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=messages,
